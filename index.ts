fe707bacd3c5baf1259a6c9ff4074e9a1d5b6d34
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-27 17:53:48 +0700

    ➕ Cài đặt TinyMCE

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import clientRoutes from "./routes/client/index.route";
 import adminRoutes from "./routes/admin/index.route";
 
 import { systemConfig } from "./config/config";
+import path from "path";
 
 const app: Express = express();
 const port: string | number = process.env.PORT || 3000;
@@ -15,7 +16,13 @@ database.connect();
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+// TinyMCE
 
+app.use(
+  "/tinymce",
+  express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
+// End TinyMCE
 // tạo biến toàn cục
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
