4a1b4f9182e950c41cf4b00d704ddfa6f5a3bec9
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-27 17:35:03 +0700

    ✨ Layout chung bên Admin - tạo route cơ bản

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express, { Express } from "express";
 import * as database from "./config/database";
 import dotenv from "dotenv";
+
 import clientRoutes from "./routes/client/index.route";
+import adminRoutes from "./routes/admin/index.route";
 
 const app: Express = express();
 const port: string | number = process.env.PORT || 3000;
@@ -15,6 +17,7 @@ app.set("view engine", "pug");
 app.use(express.static("public"));
 
 clientRoutes(app);
+adminRoutes(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
