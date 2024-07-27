d59681a70c8557d03d83ddb610dead4b19e14494
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-27 22:07:52 +0700

    ♻️ Tạo helper upload to cloudinary

--- middlewares/admin/uploadCloud.middleware.ts
+++ middlewares/admin/uploadCloud.middleware.ts
@@ -1,36 +1,5 @@
-import { v2 as cloudinary } from "cloudinary";
-import streamifier from "streamifier";
 import { Request, Response, NextFunction } from "express";
-import dotenv from "dotenv";
-dotenv.config();
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_KEY,
-  api_secret: process.env.CLOUD_SECRET,
-});
-
-let streamUpload = (buffer) => {
-  return new Promise((resolve, reject) => {
-    let stream = cloudinary.uploader.upload_stream(
-      {
-        resource_type: "auto", // Tự động nhận dạng loại file
-      },
-      (error, result) => {
-        if (result) {
-          resolve(result);
-        } else {
-          reject(error);
-        }
-      }
-    );
-
-    streamifier.createReadStream(buffer).pipe(stream);
-  });
-};
-export const uploadToCloudinary = async (buffer: any) => {
-  let result = (await streamUpload(buffer)) || "";
-  return result["url"];
-};
+import { uploadToCloudinary } from "../../helpers/uploadToCloudinary";
 export const uploadSingle = async (
   req: Request,
   res: Response,
